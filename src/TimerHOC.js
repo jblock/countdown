91c379ee397bf606c8e77db8df6f89443fa4c50f
Jason Block <jasonblock@Cinco-Fone.local>
2017-08-10 22:58:12 -0500

    WIP

--- src/TimerHOC.js
+++ src/TimerHOC.js
@@ -3,28 +3,43 @@ import moment from 'moment'
 
 const TimerHOC = (Component) => {
   return class TimerHOCInner extends React.Component {
-    componentDidMount() {
+    create() {
+      if (this.timer) return
       this.timer = setInterval(() => {
         this.forceUpdate()
       }, 1000)
     }
-    componentWillUnmount() {
+    destroy() {
+      if (!this.timer) return
       clearInterval(this.timer)
+      delete this.timer
+    }
+    componentDidMount() {
+      this.create()
+    }
+    componentWillUnmount() {
+      this.destroy()
+    }
+    componentDidUpdate() {
+      if (this.props.on) {
+        this.create()
+      } else {
+        this.destroy()
+      }
     }
     render(){
       const {end} = this.props
       if (!end) return <Component {...this.props} />
 
       const now = moment()
-      const diff = moment.duration(end.diff(now))
-      const secs = diff.asSeconds()
+      const diff = moment.duration(end.diff(now)).asSeconds()
       
-      const hours = (secs / 60 / 60) % 24
-      const minutes = (secs / 60) % 60
-      const seconds = (secs) % 60
+      const hours = (diff / 60 / 60) % 24
+      const minutes = (diff / 60) % 60
+      const seconds = (diff) % 60
 
       return (
-        <Component {...this.props} hours={hours} minutes={minutes} seconds={seconds} />
+        <Component {...this.props} diff={diff} hours={hours} minutes={minutes} seconds={seconds} />
       )
     }
   }
